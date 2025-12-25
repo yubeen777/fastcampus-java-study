c2dd54021af991886f43aeeeeb7c7315f99af8f1
yubeen777 <panda7715@naver.com>
2025-12-25 14:10:54 +0900

    TwoDimArray_study

---
+++ src/basic/array/TwoDimArrayTest.java
@@ -0,0 +1,24 @@
+package basic.array;
+public class TwoDimArrayTest {
+  public static void main(String[] args) {
+
+    // 2행 4열의 정수형 배열 생성
+    int[][] a = new int[2][4];
+
+    a[0][0] = 10;
+    a[0][1] = 20;
+    a[0][2] = 30;
+    a[0][3] = 40;
+
+    a[1][0] = 50;
+    a[1][1] = 60;
+    a[1][2] = 70;
+    a[1][3] = 80;
+
+    System.out.println(a[0][0] + "\t" + a[0][1] + "\t" + a[0][2] + "\t" + a[0][3]);
+    System.out.println(a[1][0] + "\t" + a[1][1] + "\t" + a[1][2] + "\t" + a[1][3]);
+
+
+  }
+}
+
